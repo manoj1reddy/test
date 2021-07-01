7f6b8e29647270978967ae5eb44fa025bbd504b5
Manoj Reddy <reddymanoj452@gmail.com>
2021-07-01 15:22:11 +0530

    This is in NewBranch

--- first.py
+++ first.py
@@ -1,1 +1,12 @@
 print("this is my first version.")
+print("Hello this is new branch")
+
+"""To create a new branch
+use -> git branch branchName
+
+To go to that branch use
+-> checkout branch branchName
+
+
+Iam creating this file in new branch so this is not visible in main branch
+hence we easily experiment on this code without need to modify the existing code."""
