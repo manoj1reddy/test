4dbaacbd75e170c5df27a8cc622f41fc59b27ec2
Manoj Reddy <reddymanoj452@gmail.com>
2021-07-01 16:25:28 +0530

    print undoing

--- second.py
+++ second.py
@@ -11,4 +11,7 @@ There are two commands to undo the unsaved fiels
 
 
 """this is for my commited  changes to be undo lets try
-Print("This is new commited to be undo let's try !")"""
+Print("This is new commited to be undo let's try !")
+"""
+
+print("This is new commited to be undo let's try !")
