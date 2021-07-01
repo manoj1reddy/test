04bb12f3abaebdc52d09ed8dcacf4f918664c610
Manoj Reddy <reddymanoj452@gmail.com>
2021-07-01 16:20:56 +0530

    this is for undo

--- second.py
+++ second.py
@@ -1,2 +1,9 @@
 #lets work with undoing the uncommited chnages
 # print("this is my uncommited ")
+print("this is my uncommited ")
+
+"""
+There are two commands to undo the unsaved fiels
+1 git checkout -- filename
+2 git restore filename
+"""
